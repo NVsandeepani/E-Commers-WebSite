b1a453d78b1b060f2a98e8c94bf4bc8990661636
Vihara Sandeepani <159278608+NVsandeepani@users.noreply.github.com>
2024-07-25 21:44:11 +0530

    Add files via upload

---
+++ E-CommerceWeb/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed('#multiple-text', {
+        strings: ['Every Step Tells A Story.....'],
+        typeSpeed: 50,
+        backSpeed: 50,
+        backDelay: 1000,
+        loop: true
+    });
+});
